3f10707700916e6e6d5c341c1b4c6b18e2bf9e68
Sakethganakrishna-medidi <sakethganakrishna@gmail.com>
2025-01-29 12:47:45 +0530

    Add files via upload

---
+++ Mathpow.java
@@ -0,0 +1,8 @@
+public class Mathpow {
+    public static void main(String[] args) {
+        double a =4;
+        double b = 5;
+        double c = Math.pow(a,b);
+        System.out.println(c);
+    }
+}
